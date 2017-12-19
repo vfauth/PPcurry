3e2ac4f067b879588be96ab9296afa8f36b15941
Fauth Victor <fauthvictor@gmail.com>
2017-12-19 19:58:03 +0100

    Components can be added

---
+++ PPcurry/Component.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Windows;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace PPcurry
+{
+    class Component
+    {
+        // Attributes
+
+        private int[] Pos; // The position of the component on the grid
+        private Image ComponentImage; // The Image object
+
+
+        // Accessors/Mutators
+
+        //public Card GetCardPaired() => cardPaired;
+        //public Card SetCardPaired(Card cardPaired) => this.cardPaired = cardPaired;
+
+
+        // Constructors
+
+        /// <summary>
+        /// Add one component to the board.
+        /// </summary>
+        /// <param name="x">The component abscissa.</param>
+        /// <param name="y">The component ordinate.</param>
+        public Component(int x, int y, Image image)
+        {
+            this.Pos = new int[2] { x , y };
+            this.ComponentImage = image;
+        }
+
+
+        // Methods
+    }
+}
--- PPcurry/MainWindow.xaml.cs
+++ PPcurry/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace PPcurry
 {
@@ -20,9 +21,46 @@ namespace PPcurry
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Attributes
+
+        private String ComponentToAdd;
+        private List<Component> ComponentsList;
+
+
+        // Accessors/Mutators
+
+
+        // Constructors
+
         public MainWindow()
         {
             InitializeComponent();
+            ComponentToAdd = "";
+            ComponentsList = new List<Component>();
+        }
+
+
+        // Methods
+
+        private void ComponentSelectedFromPanel(object sender, MouseButtonEventArgs e)
+        {
+            this.ComponentToAdd = (String)((Image)sender).Tag;
+        }
+
+        private void MainCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (ComponentToAdd != "")
+            {
+                Image NewImage = new Image();
+                NewImage.Source = new BitmapImage(new Uri($"pack://siteoforigin:,,,/Resources/{ComponentToAdd}.png"));
+                NewImage.Height = 100; // TO BE IMPROVED: must not be fixed
+                NewImage.Width = 100;
+                NewImage.SetValue(Canvas.LeftProperty, (e.GetPosition(MainCanvas)).X); // Place the component at the click position, relative to the canvas
+                NewImage.SetValue(Canvas.TopProperty, (e.GetPosition(MainCanvas)).Y);
+                MainCanvas.Children.Add(NewImage);
+
+                this.ComponentToAdd = ""; // TO BE IMPROVED // Reinitialize the chosen component; must be tied to configuration (for serial insertion)
+            }
         }
     }
 }
