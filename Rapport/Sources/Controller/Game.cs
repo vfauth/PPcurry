50633fa61b169230e0a01170d1e4d0bce4434c0e
Victor Fauth <fauthvictor@gmail.com>
2018-01-06 01:48:04 +0100

    Completed the report section about memory

---
+++ Rapport/Sources/Controller/Game.cs
@@ -0,0 +1,38 @@
+﻿class Game
+{
+	// Attributes
+
+	private Card firstCardSelected; // The first selected card this turn
+	private Board board; // The board with all the cards
+	private Display display; // The GUI
+
+	
+	// Accessors/Mutators
+
+	public Board GetBoard()
+
+
+	// Constructor
+
+	/// Create a new game
+	public Game(Display display, int X, int Y)
+
+	
+	// Methods
+
+	/// Method to be executed when a card is chosen
+	public void CardChosen(object sender, EventArgs e)
+
+	/// Go to the next turn by incrementing the counter and refreshing the display
+	public void NextTurn()
+
+	/// Check whether two cards are paired
+	private bool CheckPair(Card card1, Card card2)
+
+	/// Check whether the game is finished or not
+	private bool IsFinished()
+
+	/// Exit the game
+	public void Exit(object sender)
+	
+}
---
+++ Rapport/Sources/Model/Card.cs
@@ -0,0 +1,23 @@
+﻿class Card
+{
+	// Attributes
+	
+	private Card cardPaired; // The other card in the pair
+	private int pair; // The id of the pair
+	private bool isDisplayed; // Whether the card is currently displayed
+	private bool isFound; // Whether this card's pair has been found
+
+	
+	// Accessors/Mutators
+	
+	/*
+	[...]
+	*/
+
+	
+	// Constructor
+	
+	/// Create one card
+    public Card(int pair)
+	
+}
