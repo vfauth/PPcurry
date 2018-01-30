339d5ebeee61105e5536946094ed28bf722f5f50
Fauth Victor <fauthvictor@gmail.com>
2018-01-30 20:44:20 +0100

    Hotkeys for rotation implemented.

--- PPcurry/MainWindow.xaml.cs
+++ PPcurry/MainWindow.xaml.cs
@@ -76,6 +76,20 @@ namespace PPcurry
         }
 
         /// <summary>
+        /// Defines the hotkeys.
+        /// </summary>
+        private void AddHotKeys()
+        {
+            RoutedCommand leftRotation = new RoutedCommand();
+            leftRotation.InputGestures.Add(new KeyGesture(Key.E));
+            CommandBindings.Add(new CommandBinding(leftRotation, LeftRotationButton_Click));
+
+            RoutedCommand rightRotation = new RoutedCommand();
+            rightRotation.InputGestures.Add(new KeyGesture(Key.R));
+            CommandBindings.Add(new CommandBinding(rightRotation, RightRotationButton_Click));
+        }
+
+        /// <summary>
         /// If the mouse moves over a component in the left panel and the left mouse button is pressed, the component is dragged
         /// </summary>
         private void ComponentInLeftPanel_MouseMove(object sender, MouseEventArgs e)
@@ -98,7 +112,23 @@ namespace PPcurry
         }
 
         /// <summary>
-        /// Handler called when the left rotation button is clicked.
+        /// Handler called when a key is released
+        /// </summary>
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.E:
+                    LeftRotationButton_Click(sender, e);
+                    break;
+                case Key.R:
+                    RightRotationButton_Click(sender, e);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Handler called when the left rotation button is clicked or when the E key is pressed.
         /// </summary>
         private void LeftRotationButton_Click(object sender, RoutedEventArgs e)
         {
@@ -109,7 +139,7 @@ namespace PPcurry
         }
 
         /// <summary>
-        /// Handler called when the right rotation button is clicked.
+        /// Handler called when the right rotation button is clicked or when the R key is pressed.
         /// </summary>
         private void RightRotationButton_Click(object sender, RoutedEventArgs e)
         {
