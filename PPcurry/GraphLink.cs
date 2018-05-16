f8bd32465517c4671c62c089fb77ab953cc58eef
Fauth Victor <fauthvictor@gmail.com>
2018-05-16 23:37:04 +0200

    Hotfix: some files were not committed correctly.

---
+++ PPcurry/GraphLink.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PPcurry;
+using System.Diagnostics;
+
+namespace PPcurry
+{
+    class GraphLink
+    {
+        #region Attributes
+        public Component LinkComponent { get; set; } // The component constituting the link
+        public Tuple<GraphNode, GraphNode> ConnectedNodes { get; } // The nodes linked by this link
+        #endregion
+
+
+        #region Constructor
+        public GraphLink(GraphNode node1, GraphNode node2, Component component)
+        {
+            LinkComponent = component;
+            ConnectedNodes = new Tuple<GraphNode, GraphNode>(node1, node2);
+        }
+        #endregion
+    }
+}
